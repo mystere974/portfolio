a4dc632304f0d6dd489d573837fd6425e237fba2
mystere974 <m.loghann@gmail.com>
2021-09-21 23:50:40 +0200

    ajout icons et display none

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,26 @@
 import './Home.css'
-
+import 'boxicons'
 
 function Home () {
+    const dayNight = document.querySelector('.dayNight');
+    const menuToggle = document.querySelector('.menuToggle');
+    const body = document.querySelector('body');
+    dayNight.onclick = function(){
+        body.classList.toggle('dark')
+    }
     return (
-        <>
-        </>
+       <header>
+           <div className="rightSide">
+               <div className="btn dayNight">
+               <box-icon name='moon' ></box-icon>
+               <box-icon name='sun' ></box-icon>
+               </div>
+               <div className="btn menuToggle">
+               <box-icon name='menu'></box-icon>
+               <box-icon name='exit'></box-icon>
+               </div>
+           </div>
+       </header>
     )
 }
 export default Home;
