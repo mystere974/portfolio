5aa70307b1c7fb47f0b185e4a37c6fdb4faf5bdd
mystere974 <m.loghann@gmail.com>
2021-10-25 00:20:58 +0200

    other method to have parallax with my banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { parallaxBanner, ParallaxProvider } from "react-scroll-parallax"
 import { Background, Parallax } from "react-parallax";
 import parallaxVideo from "../parallaxVideo";
@@ -13,38 +13,16 @@ import 'boxicons'
 
 function Home () {
     const [darkMode, setDarkMode] = useState(false)
-    const video = {
-      playsInline: "playsinline",
-      autoPlay:"autoplay",
-      loop:"loop",
-      muted:"muted"
-    }
-    const source ={
-      src:{film}
-    }
-    const inline = {
-      left: '50%',
-      top: '50%',
-      position: 'absolute',
-      transform: 'translate(-50%, -50%)',
-    }
+    const [offsetY, setOffsetY] = useState(0)
+    const handleScroll = () => setOffsetY(window.pageYOffset)
+
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll)
+    }, []);
     
     return (
-      <>
-<Parallax strength={500} bgImage={mask}>
-  <div style={{ height: 500}}>
-    <Background className="main">
-  <video playsInline="playsinline" autoPlay="autoplay" loop="loop" muted="muted">
-               <source src={film} />
-           </video>
-           
-           <h2>LOGHANN MUSSARD</h2>
-    <div style={inline}>LOGHANN MUSSARD</div>
-      </Background>
-  </div>
-</Parallax>
-  
-
+<>
         <div className={darkMode ? "dark-mode" : "light-mode"}>
       <div className="container">
         <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
@@ -56,17 +34,17 @@ function Home () {
         </div>
         <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
       </div>
-       <section className="main">
+       <section className="main" style= {{ transform: `translateY(${offsetY * 0.1}px)`}}>
            <video playsInline="playsinline" autoPlay="autoplay" loop="loop" muted="muted">
                <source src={film} />
            </video>
            {darkMode ? <img src={mask1} className="mask1" alt="mask1" /> : <img src={mask} className="mask" alt="mask" />}
            <h2>LOGHANN MUSSARD</h2>
        </section>
-
        <div>
          About Me
-         </div>
+         Le LContrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.orem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.1615651651
+62 65165        </div>
        </div>
        
        </>
